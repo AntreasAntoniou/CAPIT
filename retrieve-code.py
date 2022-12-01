4e07c9c58c44da20965493bec173b174c41df578
Antreas Antoniou <iam@antreas.io>
2022-12-01 07:02:39 +0000

    Develop demo UI

--- retrieve-code.py
+++
@@ -1,8 +0,0 @@
-import wandb
-
-run = wandb.init()
-artifact = run.use_artifact(
-    "machinelearningbrewery/CAPInstagramImageTextFiltered/project-source:v13",
-    type="code",
-)
-artifact_dir = artifact.download()
